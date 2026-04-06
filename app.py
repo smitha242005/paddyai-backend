c1f555225916d60d1702398189db006359f9459c
smitha242005 <smitha.j2025@vitstudent.ac.in>
2026-04-06 21:18:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,8 +19,8 @@ CORS(app, origins=[
 ])
 
 # ── Auto Download disease_model.tflite from Google Drive ──
-DISEASE_MODEL_PATH = 'disease_model_v2.tflite'
-GDRIVE_FILE_ID = '1hdJ4335ahExg3joOMVReYurgQN7BDHjP'
+DISEASE_MODEL_PATH = 'disease_model_v3.tflite'
+GDRIVE_FILE_ID = '19A-Rgsn00lnCpytWzH52nLLnFcJAtdCk'
 
 if not os.path.exists(DISEASE_MODEL_PATH):
     print("⬇️  Downloading disease_model.tflite from Google Drive...")
